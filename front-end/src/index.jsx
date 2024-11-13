cfec35875361195377a58ac7ec57212bf977145f
zacharyzhang1208 <zacharyzhang1208@hotmail.com>
2024-11-13 23:56:22 +0800

    Simple Route

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import LoginTable from "./components/LoginTable/Index.jsx"
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function main(){
     const root = createRoot(document.getElementById("app"));
     root.render(
-        <LoginTable />
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/">Nothing</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<LoginTable/>} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
