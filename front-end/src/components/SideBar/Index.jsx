bec6719e77afeb727a1c8ce22fb5a61faa26c46b
zacharyzhang1208 <zacharyzhang1208@hotmail.com>
2024-11-19 13:04:03 +0800

    optimize css

--- front-end/src/components/SideBar/Index.jsx
+++ front-end/src/components/SideBar/Index.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import '../../css/sidebar.css'
+import { Link, useLocation } from 'react-router-dom';
+import { FaHome, FaBoxes, FaCog } from 'react-icons/fa';
+import '../../css/sidebar.css';
 
 const Sidebar = () => {
+    const location = useLocation();
+
+    const menuItems = [
+        {
+            path: '/',
+            name: 'Home',
+            icon: <FaHome className="sidebar-icon" />
+        },
+        {
+            path: '/inventory',
+            name: 'Inventory',
+            icon: <FaBoxes className="sidebar-icon" />
+        },
+        {
+            path: '/settings',
+            name: 'Settings',
+            icon: <FaCog className="sidebar-icon" />
+        }
+    ];
+
     return (
         <div className="sidebar">
+            <div className="sidebar-header">
+                <h1>Diamond App</h1>
+            </div>
             <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/inventory">Inventory</Link>
-                </li>
+                {menuItems.map((item) => (
+                    <li 
+                        key={item.path}
+                        className={`sidebar-item ${location.pathname === item.path ? 'active' : ''}`}
+                    >
+                        <Link to={item.path}>
+                            {item.icon}
+                            <span>{item.name}</span>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </div>
     );
