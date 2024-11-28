04171e7e68df9f7d8946402efed84ac69ac8253b
RootCoinT <rootcointonny@gmail.com>
2024-11-28 14:34:20 +0800

    CA_register

--- backend/config/CA.js
+++
@@ -1,7 +0,0 @@
-// CA的公钥配置
-export const CA_CONFIG = {
-    publicKey: `-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAivfbJNYmWynKtI1poU79\nAW8sn00+E9znnzzIcDeCvxJrM7szsSclFDGAHdvKiwxicaUGmGKgoZwKHdxhRxAB\nUF14jMLB1MyS6FPH6BEKprSB/m6Yn9GudhPbGzNhqWR4D/Mzx73TTs+MWhe0vnwg\neNmkUikb1pfUj17Xc261DjTzAH73RVgVqHPIaKbFvJszezATwc1L8gXSEXR6gz4K\nsH6BXspFY17PN9Ig+iZk84IpT59oBYzdS7yxWzom+At9adLllrQUaszm+ty0TwhY\neAdTS3xXFIzle0+f62m0a086ULcmgITEbbDIaDQs6SKUPnfYyqizRjZm6syHd7lP\nNQIDAQAB\n-----END PUBLIC KEY-----\n`,
-    algorithm: "RSA-SHA256",
-    version: "1.0",
-    issuer: "DiamondChainRootCA"
-};
--- backend/config/network.js
+++
@@ -1,4 +0,0 @@
-export const NETWORK_CONFIG = {
-    url: "http://127.0.0.1:7545",
-    chainId: 1337
-};
